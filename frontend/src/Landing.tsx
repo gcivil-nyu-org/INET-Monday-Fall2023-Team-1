d2bc94340716df28c5c4d70776291e3b4f1b6c1a
Sameer Kolhar <kolhar730@gmail.com>
2023-10-17 18:48:53 -0400

    update branch with .vercel (local) and minor page content update

--- frontend/src/Landing.tsx
+++ frontend/src/Landing.tsx
@@ -86,7 +86,7 @@ export default function Landing() {
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                         <div className="relative rounded-full px-3 py-1 text-md leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                            Pet sitting made simple and easy as 1,2,3 in NYC. {' '}
+                            Pet sitting made simple and easy as 1,2,3 in NYC {' '}
                             {/* <a href="#" className="font-semibold text-indigo-600">
                     <span className="absolute inset-0" aria-hidden="true" />
                     Read more <span aria-hidden="true">&rarr;</span>
