a200e7b9515e6f43fbc548e9156dcaab89cf38d1
Sameer Kolhar <kolhar730@gmail.com>
2023-10-29 16:14:49 -0400

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 export GIT_COMMIT_SHORT_HASH=$(git rev-parse --short HEAD)
 export GIT_COMMIT_HASH=$(git rev-parse HEAD)
 
-cd ../furbaby
+cd $ROOT_DIR/furbaby
 
 eb deploy
