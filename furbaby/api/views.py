99bb2e6f37dc81f002645b8e91c96a9d4ea34338
Yami Naik <yaminaik@yamis-air.mynetworksettings.com>
2023-11-06 12:53:35 -0500

    Add import statement

--- furbaby/api/views.py
+++ furbaby/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.generics import GenericAPIView
 from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.response import Response
 from .utils import json_response
 from api.auth_backends import EmailBackend
 from .serializers import RegistrationSerializer, UserLoginSerializer, PetSerializer
