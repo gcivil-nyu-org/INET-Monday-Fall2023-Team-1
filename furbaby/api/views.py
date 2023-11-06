aa8ba490c8118fa460631c7b88a7abfd42895729
Sameer Kolhar <kolhar730@gmail.com>
2023-11-05 20:32:08 -0500

    update with views to handle image uploads for user and pet pictures

--- furbaby/api/views.py
+++ furbaby/api/views.py
@@ -1,5 +1,5 @@
 import os
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, FileResponse
 from django.contrib.auth import login, logout
 from drf_standardized_errors.handler import exception_handler
 from rest_framework import status
@@ -8,7 +8,9 @@ from rest_framework.generics import GenericAPIView
 from django.conf import settings
 from rest_framework.decorators import api_view
 
-from .utils import json_response
+from .models import Pets
+
+from .utils import json_response, make_s3_path, read_request_body
 from api.auth_backends import EmailBackend
 from .serializers import RegistrationSerializer, UserLoginSerializer
 
@@ -17,6 +19,14 @@ from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from django_rest_passwordreset.signals import reset_password_token_created
+
+import boto3
+from botocore.exceptions import ClientError
+
+s3Config = getattr(settings, "S3_CONFIG", None)
+s3Client = boto3.client("s3", config=s3Config)
+s3AssetsFolder = getattr(settings, "ASSETS_PATH")
+s3BucketName = getattr(settings, "AWS_BUCKET_NAME")
 
 
 class UserRegistrationView(GenericAPIView):
@@ -38,9 +48,7 @@ class UserRegistrationView(GenericAPIView):
         serializer = self.serializer_class(data=request.data)
 
         if not serializer.is_valid():
-            return json_response(
-                data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
+            return json_response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
         return json_response(data=serializer.data, status=status.HTTP_201_CREATED)
@@ -63,8 +71,8 @@ class UserLoginView(APIView):
         serializer = UserLoginSerializer(data=request.data)
 
         if serializer.is_valid():
-            email = serializer.validated_data["email"]
-            password = serializer.validated_data["password"]
+            email = serializer.validated_data["email"]  # type: ignore
+            password = serializer.validated_data["password"]  # type: ignore
             email_backend = EmailBackend()
             user = email_backend.authenticate(request, email=email, password=password)
             if user is not None:
@@ -77,9 +85,7 @@ class UserLoginView(APIView):
                     data={"message": "User is not found"},
                     status=status.HTTP_404_NOT_FOUND,
                 )
-        return json_response(
-            data=serializer.errors, status=status.HTTP_401_UNAUTHORIZED
-        )
+        return json_response(data=serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
 
 @api_view(["GET", "OPTIONS", "POST"])
@@ -90,17 +96,13 @@ def logout_view(request):
         )
 
     logout(request)
-    return json_response(
-        {"detail": "Successfully logged out."}, status=status.HTTP_200_OK
-    )
+    return json_response({"detail": "Successfully logged out."}, status=status.HTTP_200_OK)
 
 
 @api_view(["GET", "OPTIONS", "POST"])
 def session_view(request):
     if not request.user.is_authenticated:
-        return json_response(
-            {"isAuthenticated": False}, status=status.HTTP_401_UNAUTHORIZED
-        )
+        return json_response({"isAuthenticated": False}, status=status.HTTP_401_UNAUTHORIZED)
 
     return json_response({"isAuthenticated": True})
 
@@ -108,9 +110,7 @@ def session_view(request):
 @api_view(["GET", "OPTIONS", "POST"])
 def whoami_view(request):
     if not request.user.is_authenticated:
-        return json_response(
-            {"isAuthenticated": False}, status=status.HTTP_401_UNAUTHORIZED
-        )
+        return json_response({"isAuthenticated": False}, status=status.HTTP_401_UNAUTHORIZED)
 
     return json_response({"email": request.user.email}, status=status.HTTP_200_OK)
 
@@ -118,9 +118,7 @@ def whoami_view(request):
 @api_view(["GET", "OPTIONS", "PUT", "PATCH", "DELETE"])
 def user_view(request):
     if not request.user.is_authenticated:
-        return json_response(
-            {"isAuthenticated": False}, status=status.HTTP_401_UNAUTHORIZED
-        )
+        return json_response({"isAuthenticated": False}, status=status.HTTP_401_UNAUTHORIZED)
 
     email_backend = EmailBackend()
     if request.method == "GET":
@@ -156,9 +154,7 @@ def index(req):
     return JsonResponse(
         {
             "version": {
-                "short_hash": getattr(
-                    settings, "GIT_COMMIT_SHORT_HASH", "default-00000"
-                ),
+                "short_hash": getattr(settings, "GIT_COMMIT_SHORT_HASH", "default-00000"),
                 "hash": getattr(settings, "GIT_COMMIT_HASH", "default-00000"),
             }
         },
@@ -167,9 +163,7 @@ def index(req):
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(
-    sender, instance, reset_password_token, *args, **kwargs
-):
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
     """
     Handles password reset tokens
     When a token is created, an e-mail needs to be sent to the user
@@ -192,9 +186,7 @@ def password_reset_token_created(
 
     # render email text
     email_html_message = render_to_string("email/password_reset_email.html", context)
-    email_plaintext_message = render_to_string(
-        "email/password_reset_email.txt", context
-    )
+    email_plaintext_message = render_to_string("email/password_reset_email.txt", context)
 
     msg = EmailMultiAlternatives(
         # title:
@@ -208,3 +200,266 @@ def password_reset_token_created(
     )
     msg.attach_alternative(email_html_message, "text/html")
     msg.send()
+
+
+@api_view(["GET", "PUT", "OPTIONS"])
+def handle_profile_picture(request):
+    if not request.user.is_authenticated:
+        return json_response(
+            data={"error": "unauthenticated request. rejected"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+
+    if request.method == "GET":
+        return __get_user_profile_picture__(request)
+
+    if request.method == "PUT":
+        return __upload_profile_picture__(request)
+
+    return json_response(
+        {
+            "error": "incorrect request method",
+            "message": 'endpoint only allows only "GET" & "PUT" requests',
+        },
+        status=status.HTTP_405_METHOD_NOT_ALLOWED,
+    )
+
+
+def __get_user_profile_picture__(request):
+    profile_picture_path = make_s3_path(
+        s3AssetsFolder, request.user.id, "profile-picture", "picture"
+    )
+
+    try:
+        image_object = s3Client.get_object(Bucket=s3BucketName, Key=profile_picture_path)
+        object_content = image_object["Body"].read()
+        return HttpResponse(object_content, content_type="image/jpeg")
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            return json_response(
+                {
+                    "data": {
+                        "message": "no profile picture present with current user: {}".format(
+                            request.user.id
+                        )
+                    }
+                },
+                status=status.HTTP_200_OK,
+            )
+        return json_response(
+            data={
+                "error": "failed to fetch profile picture, ({})".format(e.__str__()),
+                "message": "unknown error occurred while fetching user profile picture",
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+def __upload_profile_picture__(request):
+    if s3Config == None:
+        return json_response(
+            data={"error": "failed to upload profile picture due to internal error"},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    picture = request.FILES["profile_picture"]
+
+    if picture == None:
+        return json_response(
+            data={
+                "error": "file missing in upload",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if picture.content_type != "image/jpeg":
+        return json_response(
+            {
+                "error": "incorrect file format uploaded",
+                "message": 'only file type of "jpeg" is accepted',
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    try:
+        upload_path = make_s3_path(s3AssetsFolder, request.user.id, "profile-picture", "picture")
+        # NOTE: if the Key is the same, the object(s) are overwritten
+        s3Client.put_object(Bucket=s3BucketName, Body=picture, Key=upload_path)
+    except ClientError as e:
+        return json_response(
+            {
+                "error": e.__str__(),
+                "message": "failed to upload file",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    return json_response(
+        data={"message": "profile picture has been updated"},
+        status=status.HTTP_201_CREATED,
+    )
+
+
+@api_view(["POST", "PUT", "OPTIONS", "DELETE"])
+def handle_pet_pictures(request):
+    if not request.user.is_authenticated:
+        return json_response(
+            data={"error": "unauthenticated request. rejected"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+
+    if s3Config == None:
+        return json_response(
+            data={"error": "failed to upload picture due to internal error"},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    if request.method == "POST":
+        return __get_user_pet_picture__(request)
+
+    if request.method == "PUT":
+        return __put_user_pet_picture__(request)
+
+    if request.method == "DELETE":
+        return __delete_user_pet_picture__(request)
+
+    return json_response(
+        {
+            "error": "incorrect request method",
+            "message": 'endpoint only allows only "POST" & "PUT" requests',
+        },
+        status=status.HTTP_405_METHOD_NOT_ALLOWED,
+    )
+
+
+def __get_user_pet_picture__(request):
+    pet_id = request.data["pet_id"]
+
+    pet_info = Pets.objects.filter(name=pet_id, owner=request.user.id).first()
+
+    if pet_info == None:
+        return json_response(
+            data={
+                "error": "failed to locate pet with given name and user",
+            },
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    pet_picture_path = make_s3_path(s3AssetsFolder, request.user.id, "pets", str(pet_info.id))
+
+    try:
+        image_object = s3Client.get_object(Bucket=s3BucketName, Key=pet_picture_path)
+        object_content = image_object["Body"].read()
+        return HttpResponse(object_content, content_type="image/jpeg")
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            return json_response(
+                {
+                    "data": {
+                        "message": "no pet picture present with current user({}) and pet({})".format(
+                            request.user.id, pet_info.name
+                        )
+                    }
+                },
+                status=status.HTTP_200_OK,
+            )
+        return json_response(
+            data={
+                "error": "failed to fetch pet picture, ({})".format(e.__str__()),
+                "message": "unknown error occurred while fetching pet profile picture",
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+def __put_user_pet_picture__(request):
+    pet_id = request.data["pet_id"]
+
+    pet_info = Pets.objects.filter(name=pet_id, owner=request.user.id).first()
+
+    if pet_info == None:
+        return json_response(
+            {"error": "failed to locate pet in user account"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    picture = request.FILES["picture"]
+
+    if picture == None:
+        return json_response(
+            data={
+                "error": "file missing in upload",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if picture.content_type != "image/jpeg":
+        return json_response(
+            {
+                "error": "incorrect file format uploaded",
+                "message": 'only file type of "jpeg" is accepted',
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    try:
+        upload_path = make_s3_path(s3AssetsFolder, request.user.id, "pets", str(pet_info.id))
+        # NOTE: if the Key is the same, the object(s) are overwritten
+        s3Client.put_object(Bucket=s3BucketName, Body=picture, Key=upload_path)
+    except ClientError as e:
+        return json_response(
+            {
+                "error": e.__str__(),
+                "message": "failed to upload file",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    return json_response(
+        data={"message": "pet picture has been updated"},
+        status=status.HTTP_201_CREATED,
+    )
+
+
+def __delete_user_pet_picture__(request):
+    pet_id = request.data["pet_id"]
+
+    pet_info = Pets.objects.filter(name=pet_id, owner=request.user.id).first()
+
+    if pet_info == None:
+        return json_response(
+            data={
+                "error": "failed to locate pet with given name and user",
+            },
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    pet_picture_path = make_s3_path(s3AssetsFolder, request.user.id, "pets", str(pet_info.id))
+
+    try:
+        s3Client.delete_object(Bucket=s3BucketName, Key=pet_picture_path)
+        return json_response(
+            data={
+                "data": {"message": "deleted pet({}) picture successful".format(pet_info.name)},
+            },
+            status=status.HTTP_200_OK,
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            return json_response(
+                {
+                    "data": {
+                        "message": "no pet picture present with current user({}) and pet({})".format(
+                            request.user.id, pet_info.name
+                        )
+                    }
+                },
+                status=status.HTTP_200_OK,
+            )
+        return json_response(
+            data={
+                "error": "failed to fetch pet picture, ({})".format(e.__str__()),
+                "message": "unknown error occurred while fetching pet picture",
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
