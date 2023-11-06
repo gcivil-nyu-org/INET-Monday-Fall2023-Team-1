5ba1d0c673d2561009a66b0188764dd1dabb483a
Sameer Kolhar <kolhar730@gmail.com>
2023-11-05 20:34:03 -0500

    update urls to ensure new picture based views are accessible

--- furbaby/api/urls.py
+++ furbaby/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 
 from . import views
 
-# csrf
+# NOTE: We might have to use the decorator csrf_protect to ensure that
+# the endpoints that need the csrf token always get it
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -20,4 +21,14 @@ urlpatterns = [
         include("django_rest_passwordreset.urls", namespace="password_reset"),
     ),
     path("api/user", views.user_view, name="user-info"),
+    path(
+        "api/user/profile_picture",
+        views.handle_profile_picture,
+        name="user-info-profile-picture",
+    ),
+    path(
+        "api/user/pet/pictures",
+        views.handle_pet_pictures,
+        name="user-info-pet-pictures",
+    ),
 ]
