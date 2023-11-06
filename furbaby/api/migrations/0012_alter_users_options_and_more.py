8a24d8a9cacbe8d14fd377dfcf1f02f8d2a12a27
Sameer Kolhar <kolhar730@gmail.com>
2023-11-06 13:23:25 -0500

    apply black formatter changes

--- furbaby/api/migrations/0012_alter_users_options_and_more.py
+++ furbaby/api/migrations/0012_alter_users_options_and_more.py
@@ -4,18 +4,22 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('api', '0011_remove_pets_pictures_remove_users_profile_picture_and_more'),
+        ("api", "0011_remove_pets_pictures_remove_users_profile_picture_and_more"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='users',
+            name="users",
             options={},
         ),
         migrations.AddConstraint(
-            model_name='users',
-            constraint=models.CheckConstraint(check=models.Q(('date_of_birth__gte', 16), ('date_of_birth__isnull', True), _connector='OR'), name='check_if_atleast_16_constraint'),
+            model_name="users",
+            constraint=models.CheckConstraint(
+                check=models.Q(
+                    ("date_of_birth__gte", 16), ("date_of_birth__isnull", True), _connector="OR"
+                ),
+                name="check_if_atleast_16_constraint",
+            ),
         ),
     ]
