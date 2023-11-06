70d4409808b2399f7435101e7cc2f0a1a4d9a87d
Sameer Kolhar <kolhar730@gmail.com>
2023-11-05 20:28:46 -0500

    add new utils read_request_body and make_s3_path

--- furbaby/api/utils.py
+++ furbaby/api/utils.py
@@ -1,5 +1,16 @@
+import json
 from django.http import JsonResponse
 
 
 def json_response(data=None, status=None):
     return JsonResponse(data={"data": data}, status=status)
+
+
+def make_s3_path(*args: str):
+    return "/".join(args)
+
+
+def read_request_body(request):
+    body_unicode = request.body.decode("utf-8")
+    body = json.loads(body_unicode)
+    return body["content"]
